2a36831c9c44597305a8f820aa2bdb31027f8fbf
godcandidate <godcandidate@gmail.com>
2024-05-31 10:44:27 +0000

    create server.js
    Handles node express setup

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import routes from './route/routes.js';
+
+import bodyParser from 'body-parser';
+
+const app = express();
+
+/* middleware */
+app.use(express.json());
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true })); // For parsing form data
+app.use(bodyParser.json());
+
+const port = 8080;
+
+// Mount the routes
+app.use('/', routes); 
+
+try {
+    app.listen(port, () => {
+        console.log(`Server connected to http://localhost:${port}`);
+    });
+} catch (error) {
+    console.log("Cannot connect to the server");
+}
+
