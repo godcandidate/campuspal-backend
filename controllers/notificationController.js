4bc054ceb10a69d728222f66cef5f8f11b46129a
godcandidate <godcandidate@gmail.com>
2024-07-26 12:28:29 +0000

    update notificationController.js
    Added a function to get user notifications

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -1,7 +1,7 @@
 import { arrayUnion, arrayRemove, query,doc, setDoc, addDoc, updateDoc, getDoc, getDocs, collection, deleteDoc, where} from "firebase/firestore";
 import db from "../firestore.js";
 
-
+// Notify claimaint and finder when there is a match
 export async function cardMatchNotifier(req, res, item, founderID){
     try {
         // Get user data and id
@@ -45,3 +45,36 @@ export async function cardMatchNotifier(req, res, item, founderID){
         return res.status(500).send({ error: "Sending notifications failed on firebase" });
      }  
   }
+
+// Get all user notifications
+export async function getUserNotifications(req, res){
+    try {
+        // Get business data and id
+        const {userId} = req.user;
+        let notificationRef = collection(db, "notifications");
+
+        // Query for products where creator field matches userId
+        const q = query(notificationRef, where("recipientId", "==", userId));
+      
+        const notificationSnap = await getDocs(q);
+       
+        // Check if user has products
+        if (!notificationSnap.docs) {
+            return res.status(404).send({ error: "No notifications" });   
+        }
+
+        const notifications = notificationSnap.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+          }));
+
+        return res.status(200).send(notifications);
+        
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({ error: "Retrieving user notifications from firebase failed" });
+    }  
+}
+
+
